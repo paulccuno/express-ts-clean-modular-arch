d2324d674d311ac2cac9275928ccf073574c9827
paulccuno <pallccuno@gmail.com>
2025-05-19 11:15:03 -0500

    corregir mensaje de documentacion

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import { env } from './config/env';
 bootstrapDatabase().then(() => {
   app.listen(env.PORT, () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${env.PORT}`);
-    console.log(`📚 Swagger UI: http://localhost:${env.PORT}/api/docs`);
+    console.log(`📚 Swagger UI: http://localhost:${env.PORT}/docs`);
   });
 });
